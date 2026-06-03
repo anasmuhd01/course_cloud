4ba72d6bd1d49c5cdba091113d1670f8eecef5fb
vakanda-001 <midastradingcoind@gmail.com>
2026-06-03 22:00:38 +0530

    wishlist model

--- student/models.py
+++ student/models.py
@@ -15,3 +15,10 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     total = models.DecimalField(max_digits=10,decimal_places=2)
 
+class Wishlist(models.Model):
+    course_object = models.ForeignKey(Course,on_delete=models.CASCADE)
+    student_object = models.ForeignKey(User,on_delete=models.CASCADE)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+
+
