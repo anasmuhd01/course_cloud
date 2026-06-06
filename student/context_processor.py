a38fa813878cd0a51e38d8266d56892ae1ca55d7
vakanda-001 <midastradingcoind@gmail.com>
2026-06-07 00:06:52 +0530

    all

--- student/context_processor.py
+++ student/context_processor.py
@@ -4,13 +4,12 @@ def cart_count(request):
     if request.user.is_authenticated:
         cart_count = Cart.objects.filter(student_object  = request.user).count()
         wishlist_count = Wishlist.objects.filter(student_object = request.user).count()
-        qs = Order.objects.filter(is_paid = True,student_object = request.user)
-        total_order = 0
+        qs=Order.objects.filter(is_paid=True,student_object=request.user)
+        course_count=0
         for i in qs:
-            total_order += i.course_object.count()
+            course_count+=i.course_object.count()
 
-
-        return {'cart_count':cart_count,'wishlist_count':wishlist_count,'total_order':total_order}
+        return {'cart_count':cart_count,'wishlist_count':wishlist_count,'total_order':course_count}
     else:
         return {'cart_count':0,'wishlist_count':0,'total_order':0}
     
